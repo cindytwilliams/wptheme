17f3f97e47e745ad1de750e4c4c33572dd2064ff
Cindy Williams <cindy.williams@volstate.edu>
2018-04-15 19:00:27 -0500

    hooked up contact form

--- wp-content/themes/cindytheme/index.php
+++ wp-content/themes/cindytheme/index.php
@@ -34,7 +34,6 @@
 
          <div class="col-lg-12 text-center">
 
-
           <?php
             // query for the about page
             $your_query = new WP_Query( 'pagename=about' );
@@ -96,35 +95,18 @@
       </div>
       <div class="row">
         <div class="col-lg-12">
-          <form id="contactForm" name="sentMessage" novalidate>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input class="form-control" id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name.">
-                  <p class="help-block text-danger"></p>
-                </div>
-                <div class="form-group">
-                  <input class="form-control" id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address.">
-                  <p class="help-block text-danger"></p>
-                </div>
-                <div class="form-group">
-                  <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required data-validation-required-message="Please enter your phone number.">
-                  <p class="help-block text-danger"></p>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <textarea class="form-control" id="message" placeholder="Your Message *" required data-validation-required-message="Please enter a message."></textarea>
-                  <p class="help-block text-danger"></p>
-                </div>
-              </div>
-              <div class="clearfix"></div>
-              <div class="col-lg-12 text-center">
-                <div id="success"></div>
-                <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-              </div>
-            </div>
-          </form>
+
+          <?php
+            // query for the about page
+            $your_query = new WP_Query( 'pagename=contact' );
+            // "loop" through query (even though it's just one page) 
+            while ( $your_query->have_posts() ) : $your_query->the_post();
+                the_content();
+            endwhile;
+            // reset post data (important!)
+            wp_reset_postdata();
+          ?>
+
         </div>
       </div>
     </div>
